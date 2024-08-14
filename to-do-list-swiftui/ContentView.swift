b57bfdc860d5312bb787a088ec527eeb4763fd11
Fernando Schulz <fernando.rsl3@gmail.com>
2024-08-14 18:45:09 -0300

    Initial Commit

---
+++ to-do-list-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  to-do-list-swiftui
+//
+//  Created by Fernando Schulz on 14/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ to-do-list-swiftui/to_do_list_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_do_list_swiftuiApp.swift
+//  to-do-list-swiftui
+//
+//  Created by Fernando Schulz on 14/08/24.
+//
+
+import SwiftUI
+
+@main
+struct to_do_list_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
